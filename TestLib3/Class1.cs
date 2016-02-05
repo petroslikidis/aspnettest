359831f1904793f0460b57261e14685603a640f5
Petros Likidis <petros.likidis@gmail.com>
2016-02-05 14:09:40 +0000

    Initial commit

---
+++ TestLib3/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLib3
+{
+    public class Class1
+    {
+        public static string[] GetValues()
+        {
+            return new string[] { "value1", "value2", "value3", "value4" };
+        }
+    }
+}
